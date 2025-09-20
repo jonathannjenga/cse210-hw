489262cfa247c95b7f488d2c1be89f144bc6dbe7
Jonathan Njenga <jonathannjenga.github.jonathannjenga@yahoo.com>
2025-09-20 19:25:04 +0300

    Week 1 Exercise 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("what is your last name");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"your name is {firstName} {lastName} {firstName}");
+        
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,58 @@
-using System;
+using Sys
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.WriteLine("what is your grade percent?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string letter = "";
+
+        if (percent >= 90)
+
+
+        }
+        {
+         letter = "A";
+        }    
+
+
+        else if (percent >= 80)
+
+        {
+
+          letter = "B";
+        }
+           else if(percent >= 70)
+        {
+           letter = "C";
+     
+        }
+            else if(percent >= 60)
+     
+        {
+            letter = "D";
+     
+        }
+             else
+     
+        {
+             letter = "F";
+     
+        }
+             Console.WriteLine($"your grade is: {letter}");
+             
+        if (percent >= 70)
+
+{
+    Console.Write("you have passed!");
 }
+
+else
+{
+    Console.WriteLine("Better luck next time");
+
+}
+
